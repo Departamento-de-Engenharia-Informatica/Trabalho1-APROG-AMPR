99c7b04b66eff7dc95c8cb5a470f0c1c44d8e469
1241225-AfonsoMota <1241225@isep.ipp.pt>
2024-11-19 12:11:19 +0000

    Leitura de Dados

--- main.java
+++ main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Testsess");
+
+        //=======Declaração de variáveis=======
+        String textoDesc;
+        int l,c;
+        Scanner ler = new Scanner(System.in);
+
+        //===========Leitura de Dados==========
+        textoDesc = ler.nextLine();
+        l=ler.nextInt();
+        c=ler.nextInt();
+        int[][] planeamento = new int[l][c];
+        for(int i=0 ; i<l; i++) for(int j=0 ; j<c; j++) planeamento[i][j]=ler.nextInt();
+
     }
 }
