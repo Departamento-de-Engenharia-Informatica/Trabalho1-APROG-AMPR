8b277dbd0a2074a5f3db5ce11e30340e1636415f
1241225-AfonsoMota <1241225@isep.ipp.pt>
2024-11-20 09:34:01 +0000

    Atualização

--- main.java
+++ main.java
@@ -2,19 +2,6 @@ import java.util.Scanner;
 
 public class main {
 
-    public static int[][] lerPlaneamento(Scanner ler) {
-        int l = ler.nextInt();
-        int c = ler.nextInt();
-
-        int[][] planeamento = new int[l][c];
-        for (int i = 0; i < l; i++) {
-            for (int j = 0; j < c; j++) {
-                planeamento[i][j] = ler.nextInt();
-            }
-        }
-        return planeamento;
-    }
-
     public static void main(String[] args) {
 
         //=========Declaração de variáveis=========
@@ -29,6 +16,8 @@ public class main {
 
         //=========Saída do total de Kms(b)=========
         totalKms(planeamento);
+
+        //=========Recarga das Baterias(c)=========
 
     }
 
@@ -48,25 +37,27 @@ public class main {
 
     public static void SaidaPlaneamento(int[][] planeamento) {
         System.out.printf("a) planeamento (km/dia/veículo)\n");
-        System.out.printf("%-6s", "dia:");
+        System.out.printf("%-6s", "dia :");
         for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.printf("|%6d", j);
+            System.out.printf("%8d", j);
         }
         System.out.printf("%n");
-        System.out.print("------");
+
+        System.out.print("----|");
         for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.print("|------");
+            System.out.print("--------|");
         }
         System.out.printf("%n");
 
         for (int i = 0; i < planeamento.length; i++) {
-            System.out.printf("%-6s", "V" + i + ":");
+            System.out.printf("%-6s", "V" + i + "  :");
             for (int j = 0; j < planeamento[0].length; j++) {
-                System.out.printf("|%6d", planeamento[i][j]);
+                System.out.printf("%8d", planeamento[i][j]);
             }
             System.out.printf("%n");
         }
     }
+
 
     public static void totalKms(int[][] planeamento) {
         System.out.printf("\nb) total de km a percorrer\n");
@@ -77,6 +68,19 @@ public class main {
             }
             System.out.printf("V%-1d :     %4d km\n", i, totalKms);
         }
+    }
+
+    public static int[][] lerPlaneamento(Scanner ler) {
+        int l = ler.nextInt();
+        int c = ler.nextInt();
+
+        int[][] planeamento = new int[l][c];
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                planeamento[i][j] = ler.nextInt();
+            }
+        }
+        return planeamento;
     }
 
 }
