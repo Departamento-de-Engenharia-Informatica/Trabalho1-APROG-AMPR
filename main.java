1134a1f7fa161c2f9d273f7132f66fd1d4040485
1241225-AfonsoMota <1241225@isep.ipp.pt>
2024-11-29 10:19:28 +0000

    Correção de tipo de dados...

--- main.java
+++ main.java
@@ -9,13 +9,10 @@ public class main {
     public static void main(String[] args) {
 
         //=========Declaração de variáveis=========
-        int[] arrayTotalKm;
-        double[] arrayMediaKmFrota;
-        int[][] planeamento,matrizQntRecargas;
-        double[][] matrizCargaFinalDia;
+        double[] arrayTotalKm,arrayMediaKmFrota;
+        double[][] planeamento,matrizQntRecargas,matrizCargaFinalDia;
         String textoDesc;
         int l,c,ficheiro;
-
         Scanner ler = new Scanner(System.in);
 
         //===========Leitura de Dados e Saída do Planeamento (a)==========
@@ -32,7 +29,8 @@ public class main {
             System.out.println("Opção inválida.");
             return;
         }
-        SaidaPlaneamento(planeamento);
+        System.out.printf("a) planeamento (km/dia/veículo)\n");
+        SaidaMatrizes(planeamento,"a");
 
         //=========Saída do total de Kms(b)=========
         arrayTotalKm = TotalKms(planeamento);
@@ -61,13 +59,13 @@ public class main {
         //=====Veículo de Prevenção no dia X(j)=====
         VeiculoPrevencao(planeamento,matrizCargaFinalDia);
     }
-    public static void VeiculoPrevencao(int[][] planeamento, double[][] matrizCargaFinalDia) {
+    public static void VeiculoPrevencao(double[][] planeamento, double[][] matrizCargaFinalDia) {
         int veiculoPrevencao = -1;
-        int menorUso = Integer.MAX_VALUE;
+        double menorUso = Integer.MAX_VALUE;
         double maiorCarga = -1;
 
         for (int i = 0; i < planeamento.length; i++) {
-            int usoDoDia = planeamento[i][DiaX];
+            double usoDoDia = planeamento[i][DiaX];
             double cargaFinal = matrizCargaFinalDia[i][DiaX];
             if (usoDoDia < menorUso || (usoDoDia == menorUso && cargaFinal > maiorCarga) || (usoDoDia == menorUso && cargaFinal == maiorCarga && i < veiculoPrevencao)) {
                 veiculoPrevencao = i;
@@ -82,7 +80,7 @@ public class main {
             System.out.println("Nenhum veículo disponível para prevenção.");
         }
     }
-    public static void CustoRecargas(int[][] matrizQntRecargas){
+    public static void CustoRecargas(double[][] matrizQntRecargas){
         int nrRecargas=0;
         for(int i=0;i<matrizQntRecargas.length; i++){
             for(int j=0; j<matrizQntRecargas[i].length;j++)nrRecargas+=matrizQntRecargas[i][j];
@@ -90,7 +88,7 @@ public class main {
         if (nrRecargas != 0) System.out.println(String.format("\ni) custo das recargas da frota: %.2f€", nrRecargas * CUSTORECARGA));
         else System.out.println("Não existem recargas a fazer (Custo: 0€)");
     }
-    public static void DiaMaisTardio(int[][] matrizQntRecargas){
+    public static void DiaMaisTardio(double[][] matrizQntRecargas){
         boolean carregamentoGeral=false;
         int dia=0;
         while(!carregamentoGeral && dia<matrizQntRecargas[0].length){
@@ -105,7 +103,7 @@ public class main {
         else System.out.println("\nh) Não há dia em que todos os veículos necessitam de recarga.");
 
     }
-    public static void CarrDiasConsec(int[][] matrizQntRecargas){
+    public static void CarrDiasConsec(double[][] matrizQntRecargas){
         int dias,diasVeiculo,maiorSeq=0;
         String veiculos="";
         for(int i=0;i<matrizQntRecargas.length; i++){
@@ -130,7 +128,7 @@ public class main {
         if(maiorSeq>0) System.out.println(maiorSeq + " dias consecutivos, veículos : " + veiculos);
         else System.out.println("Os veículos não carregam dias consecutivos");
     }
-    public static void DeslAcimaMedia(int[][] planeamento, double[]arrayMediaKmFrota){
+    public static void DeslAcimaMedia(double[][] planeamento, double[]arrayMediaKmFrota){
         boolean acima;
         String veiculos="";
         int count=0;
@@ -146,39 +144,32 @@ public class main {
         if(count>=1) System.out.println(count + " veículos : "+ veiculos);
         else System.out.println("Não existem veículos com deslocações sempre acima da média diária");
     }
-    public static double[] MediaKmFrota(int[][] planeamento) {
+    public static double[] MediaKmFrota(double[][] planeamento) {
         double[] arrayMediaKmFrota = new double[planeamento[0].length];
-        //OBTER ARRAY
+        // OBTER ARRAY
         for (int j = 0; j < planeamento[0].length; j++) {
-            int soma = 0;
+            double soma = 0;
             for (int i = 0; i < planeamento.length; i++) {
                 soma += planeamento[i][j];
             }
-            arrayMediaKmFrota[j] = (double) soma / planeamento.length;
+            arrayMediaKmFrota[j] = soma / planeamento.length;
         }
-        //VISUALIZAR ARRAY
+
+        double[][] matrizTemp = new double[1][arrayMediaKmFrota.length];
+        for (int i = 0; i < arrayMediaKmFrota.length; i++) {
+            matrizTemp[0][i] = arrayMediaKmFrota[i];
+        }
+        //VISUALIZAR
         System.out.println("\ne) Média de km diários da frota:");
-        System.out.printf("%4s", "Dia:");
-        for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.printf(" %8d", j);
-        }
-        System.out.println();
-        System.out.print("----|");
-        for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.print("--------|");
-        }
-        System.out.println();
-        System.out.printf("Km  :");
-        for (int j = 0; j < arrayMediaKmFrota.length; j++) {
-            System.out.printf(" %7.1f ", arrayMediaKmFrota[j]);
-        }
-        System.out.println();
+        SaidaMatrizes(matrizTemp, "e");
 
         return arrayMediaKmFrota;
     }
-    public static int[][] RecargaBaterias(int[][] planeamento) {
+
+
+    public static double[][] RecargaBaterias(double[][] planeamento) {
         int cargaAtual, nrCargas;
-        int[][] matrizQntRecargas = new int[planeamento.length][planeamento[0].length];
+        double[][] matrizQntRecargas = new double[planeamento.length][planeamento[0].length];
         //OBTER MATRIZ
         for (int i = 0; i < planeamento.length; i++) {
             cargaAtual = 100;
@@ -196,43 +187,29 @@ public class main {
         }
 
         //VISUALIZAR MATRIZ
-        System.out.println("\nc) Recargas das baterias:");
-        System.out.printf("%4s", "dia:");
-        for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.printf(" %8d", j);
-        }
         System.out.println();
-        System.out.print("----|");
-        for (int j = 0; j < planeamento[0].length; j++) {
-            System.out.print("--------|");
-        }
-        System.out.println();
-        for (int i = 0; i < matrizQntRecargas.length; i++) {
-            System.out.printf("V%d  :", i);
-            for (int j = 0; j < matrizQntRecargas[i].length; j++) {
-                System.out.printf(" %7d ", matrizQntRecargas[i][j]);
-            }
-            System.out.println();
-        }
-
+        System.out.println("c) recargas das baterias");
+        SaidaMatrizes(matrizQntRecargas,"c");
         return matrizQntRecargas;
     }
-    public static int[] TotalKms(int[][] planeamento) {
+    public static double[] TotalKms(double[][] planeamento) {
         System.out.printf("\nb) total de km a percorrer\n");
-        int[] totalKmArray = new int[planeamento.length];
+        double[] totalKmArray = new double[planeamento.length];
         //OBTER ARRAY
         for (int i = 0; i < planeamento.length; i++) {
-            int totalKms = 0;
+            double totalKms = 0;
             for (int j = 0; j < planeamento[i].length; j++) {
                 totalKms += planeamento[i][j];
             }
             totalKmArray[i] = totalKms;
         }
         //VISUALIZAR ARRAY
-        for(int i=0;i<totalKmArray.length; i++) System.out.printf("V%-1d :     %4d km\n", i, totalKmArray[i]);
+        for (int i = 0; i < totalKmArray.length; i++) {
+            System.out.printf("V%-1d :     %4.1f km\n", i, totalKmArray[i]);
+        }
         return totalKmArray;
     }
-    public static int[][] lerPlaneamento(Scanner ler) {
+    public static double[][] lerPlaneamento(Scanner ler) {
         int l, c;
         do {
             System.out.println("Digite o número de linhas (veículos) e o número de dias (colunas):");
@@ -242,13 +219,13 @@ public class main {
             else if (c < DiaX) System.out.println("Erro: DiaX (" + DiaX + ") é maior que o número de dias (colunas).\n");
         } while (l <= 0 || c <= 0 || c < DiaX);
         System.out.println("Insira o planeamento (valores da matriz, positivos):");
-        int[][] planeamento = new int[l][c];
+        double[][] planeamento = new double[l][c];
 
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < c; j++) {
-                int valor;
+                double valor;
                 do {
-                    valor = ler.nextInt();
+                    valor = ler.nextDouble();
                     if (valor < 0) System.out.println("Erro: Valor tem de ser positivo.");
                 } while (valor < 0);
                 planeamento[i][j] = valor;
@@ -256,9 +233,7 @@ public class main {
         }
         return planeamento;
     }
-
-
-    public static double[][] CargaFinalDia(int[][] planeamento) {
+    public static double[][] CargaFinalDia(double[][] planeamento) {
         double[][] matrizCargaFinalDia = new double[planeamento.length][planeamento[0].length];
         //OBTER MATRIZ
         for (int i = 0; i < planeamento.length; i++) {
@@ -272,7 +247,9 @@ public class main {
             }
         }
         //VISUALIZAR MATRIZ
-        System.out.println("\nd) Carga final das baterias:");
+
+        System.out.println("\nd) Carga das baterias:");
+
         System.out.printf("%4s", "dia:");
         for (int j = 0; j < planeamento[0].length; j++) {
             System.out.printf(" %8d", j);
@@ -294,29 +271,34 @@ public class main {
         }
         return matrizCargaFinalDia;
     }
-    public static void SaidaPlaneamento(int[][] planeamento) {
-        System.out.printf("a) planeamento (km/dia/veículo)\n");
+    public static void SaidaMatrizes(double[][] matriz,String exercicio) {
         System.out.printf("%4s", "dia:");
-        for (int j = 0; j < planeamento[0].length; j++) {
+        for (int j = 0; j < matriz[0].length; j++) {
             System.out.printf(" %8d", j);
         }
         System.out.println();
 
         System.out.print("----|");
-        for (int j = 0; j < planeamento[0].length; j++) {
+        for (int j = 0; j < matriz[0].length; j++) {
             System.out.print("--------|");
         }
         System.out.println();
-
-        for (int i = 0; i < planeamento.length; i++) {
-            System.out.printf("V%d  :", i);
-            for (int j = 0; j < planeamento[i].length; j++) {
-                System.out.printf(" %7d ", planeamento[i][j]);
+        if(exercicio=="a" || exercicio=="c" || exercicio=="d"){
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.printf("V%d  :", i);
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.printf(exercicio.equals("c") ? " %7.0f " : (exercicio.equals("d") ? " %7.1f%% " : " %7.1f "), matriz[i][j]);
+                }
+                System.out.println();
             }
+        }
+        else if(exercicio.equals("e")){
+            System.out.printf("Km  :");
+            for (int j = 0; j < matriz[0].length; j++) System.out.printf(" %7.1f ", matriz[0][j]);
             System.out.println();
         }
     }
-    public static int[][] lerPlaneamentoFicheiro(String caminhoFicheiro) {
+    public static double[][] lerPlaneamentoFicheiro(String caminhoFicheiro) {
         try (Scanner ficheiro = new Scanner(new File(caminhoFicheiro))) {
             String descricao = ficheiro.nextLine();
             System.out.println("Descrição do ficheiro: " + descricao);
@@ -324,7 +306,7 @@ public class main {
             int l = ficheiro.nextInt();
             int c = ficheiro.nextInt();
 
-            int[][] planeamento = new int[l][c];
+            double[][] planeamento = new double[l][c];
             for (int i = 0; i < l; i++) {
                 for (int j = 0; j < c; j++) {
                     planeamento[i][j] = ficheiro.nextInt();
